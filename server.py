dccd1a6ab6f54bb4ea62637039d8d913d21bdc62
Gustavo Miranda <gustavocataldo@id.uff.br>
2022-11-18 19:13:25 -0300

    Criação dos arquivos iniciais.
    Tem uma classe servidor e uma classe cliente que se conectam por TCP.
    Sempre que um cliente novo se conecta com o servidor, o servidor faz um broadcast da tabela de todos os clientes e seus respectivos endereços e portas.
    Problema até então: quando um cliente se desconecta, o servidor não está corrigindo a tabela e fazendo o broadcast da mesma.

---
+++ server.py
@@ -0,0 +1,90 @@
+import threading
+import socket
+
+
+host = '127.0.0.1'
+port = 55557
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+clients_addresses = []
+nicknames = []
+clients_ports = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            # broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            clients_address = clients_addresses[index]
+            clients_port = clients_ports[index]
+
+            
+            nicknames.remove(nickname)
+            clients_addresses.remove(clients_address)
+            clients_ports.remove(clients_port)
+
+            broadcast(f'{nickname} left the chat'.encode('ascii'))
+
+            
+
+            tabela = 'NICKNAME             ADDRESS             PORT\n'
+
+            for i in range(len(clients)):
+                    tabela = tabela + f'{nicknames[i]}              {clients_addresses[i]}              {clients_ports[i]}\n'
+                
+
+            broadcast(tabela)
+            
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        client_port = address[1]
+        client_address = address[0]
+
+        
+        print(f'Connected to {str(address)}')
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+
+        print(f'Nickname of the client is {nickname}')
+
+        nicknames.append(nickname)
+        clients.append(client)
+        clients_addresses.append(client_address)
+        clients_ports.append(client_port)
+        
+
+        tabela = 'NICKNAME             ADDRESS             PORT\n'
+
+        for i in range(len(clients)):
+                tabela = tabela + f'{nicknames[i]}              {clients_addresses[i]}              {clients_ports[i]}\n'
+            
+
+        broadcast(tabela.encode('ascii'))
+        
+        broadcast(f'{nickname} joined the chat'.encode('ascii'))
+        client.send('Connected to the server'.encode('ascii'))
+
+        thread = threading.Thread(target= handle, args=(client,))
+        thread.start()
+
+print('Server listening')
+receive()
