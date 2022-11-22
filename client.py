630958598eec134953bc00d76c4ad8b03cee3b33
Pedro Bandoli <pedrobandoli@id.uff.br>
2022-11-22 12:53:03 -0300

    fix: /rejeitar passa a ser funcional

--- client.py
+++ client.py
@@ -13,8 +13,11 @@ frames = []
 connections_must_be_closed = threading.Event()
 nickname = None
 must_kill_audio_threads = threading.Event()
+
+
 class ConexaoEncerrada(Exception):
     pass
+
 
 instrucoes = f"""
             Instruçoes de uso:
@@ -25,17 +28,19 @@ instrucoes = f"""
 def return_audio_stream(input=False):
 
     return pyaudio.PyAudio().open(
-        format = pyaudio.paInt16,
-        channels= 1,
-        rate = 44100,
+        format=pyaudio.paInt16,
+        channels=1,
+        rate=44100,
         output=not input,
-        input = input,
+        input=input,
         frames_per_buffer=BUFFER_SIZE
     )
+
 
 def record_audio(stream):
     while True:
         frames.append(stream.read(BUFFER_SIZE))
+
 
 def stream_audio(udp_socket: socket.socket, address: tuple):
     while True:
@@ -44,7 +49,8 @@ def stream_audio(udp_socket: socket.socket, address: tuple):
                 udp_socket.sendto(frames.pop(0), address)
             except IndexError:
                 pass
-            
+
+
 def play_audio(stream):
     while True:
         if len(frames) == 10:
@@ -54,21 +60,25 @@ def play_audio(stream):
                 except IndexError:
                     pass
 
+
 def initialize_udp_socket(address: tuple) -> socket.socket:
     audio_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     audio_client_socket.bind(address)
     return audio_client_socket
+
 
 def initialize_tcp_socket() -> socket.socket:
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((HOST_IP, PORT))
     return client_socket
 
+
 def close_sockets(client_socket: socket.socket, udp_client: socket.socket):
     client_socket.shutdown(socket.SHUT_RDWR)
     udp_client.shutdown(socket.SHUT_RDWR)
     client_socket.close()
     udp_client.close()
+
 
 def handle_messages(conn: socket.socket, udp_conn: socket.socket, my_nickname: str):
     while True:
@@ -85,7 +95,8 @@ def handle_messages(conn: socket.socket, udp_conn: socket.socket, my_nickname: s
                 print(f'Endereço: {address} | Porta: {port}')
                 if nickname != my_nickname:
                     print('Enviando um convite para chamada de voz ao usuário!')
-                    udp_conn.sendto(str.encode(f'INVITE-{my_nickname}'), (address, int(port)))
+                    udp_conn.sendto(str.encode(
+                        f'INVITE-{my_nickname}'), (address, int(port)))
             else:
                 print(message)
         except Exception as exc:
@@ -93,32 +104,40 @@ def handle_messages(conn: socket.socket, udp_conn: socket.socket, my_nickname: s
             print(error_message)
             break
 
+
 def handle_udp(udp_conn: socket.socket, queue: queue.Queue):
     while True:
         try:
-            if connections_must_be_closed.is_set(): break
+            if connections_must_be_closed.is_set():
+                break
             msg, address = udp_conn.recvfrom(int(BUFFER_SIZE*2))
             if len(msg) < BUFFER_SIZE * 2:
                 msg = msg.decode('ascii').split('-')
-            if msg[0]== 'INVITE':
+            if msg[0] == 'INVITE':
                 sender_nickname = msg[1]
                 sender_address = address
-                print(f'Você foi convidado para uma ligação com o usuário {sender_nickname} {sender_address}. Deseja aceitar? ( /aceitar ou /rejeitar )')
+                print(
+                    f'Você foi convidado para uma ligação com o usuário {sender_nickname} {sender_address}. Deseja aceitar? ( /aceitar ou /rejeitar )')
                 resposta = queue.get()
                 if resposta == '/aceitar':
-                    udp_conn.sendto(f'/convite_aceito-{udp_conn.getsockname()}'.encode('ascii'), sender_address)
-                    udp_conn.sendto(f'/convite_aceito-{sender_address}'.encode('ascii'), udp_conn.getsockname())
-                
+                    udp_conn.sendto(
+                        f'/convite_aceito-{udp_conn.getsockname()}'.encode('ascii'), sender_address)
+                    udp_conn.sendto(
+                        f'/convite_aceito-{sender_address}'.encode('ascii'), udp_conn.getsockname())
+                else:
+                    udp_conn.sendto(
+                        '/rejeitar'.encode('ascii'), sender_address)
+
             elif msg[0] == '/convite_aceito':
                 address_destino = eval(msg[1])
                 my_address = udp_conn.getsockname()
                 print(f'ORIGEM: {my_address} | DESTINO: {address_destino}')
                 print('Convite aceito! Inicializando chamada de voz...')
                 initialize_audio_threads(udp_conn, address_destino)
-                
+
                 while queue.get() != '/encerrar_ligacao':
                     pass
-                
+
                 print('Ligação encerrada.')
                 must_kill_audio_threads.set()
 
@@ -127,12 +146,15 @@ def handle_udp(udp_conn: socket.socket, queue: queue.Queue):
         except Exception as exc:
             pass
 
+
 def initialize_audio_threads(udp_socket: socket.socket, address_destino: tuple):
     audio_stream = return_audio_stream(input=True)
     threading.Thread(target=record_audio, args=[audio_stream]).start()
-    threading.Thread(target=stream_audio, args=[udp_socket, address_destino]).start()
+    threading.Thread(target=stream_audio, args=[
+                     udp_socket, address_destino]).start()
     output_audio_stream = return_audio_stream(input=False)
     threading.Thread(target=play_audio, args=[output_audio_stream]).start()
+
 
 def handle_input(tcp_socket: socket.socket, queue: queue.Queue):
     while True:
@@ -143,10 +165,12 @@ def handle_input(tcp_socket: socket.socket, queue: queue.Queue):
             print('Conexao encerrada')
             break
 
+
 def client() -> None:
     try:
         client_socket = initialize_tcp_socket()
-        udp_client_socket = initialize_udp_socket(address=client_socket.getsockname())
+        udp_client_socket = initialize_udp_socket(
+            address=client_socket.getsockname())
         nickname_valid: bool = False
         print(instrucoes)
 
@@ -161,23 +185,25 @@ def client() -> None:
                 client_socket.send(nickname.encode('ascii'))
             else:
                 nickname_valid = True
-        
+
         if nickname_valid:
             msg_queue = queue.Queue()
 
-            threading.Thread(target=handle_messages, args=[client_socket, udp_client_socket, nickname]).start()
-            threading.Thread(target=handle_input, args=[client_socket, msg_queue]).start()            
-            threading.Thread(target=handle_udp, args=[udp_client_socket, msg_queue]).start()
+            threading.Thread(target=handle_messages, args=[
+                             client_socket, udp_client_socket, nickname]).start()
+            threading.Thread(target=handle_input, args=[
+                             client_socket, msg_queue]).start()
+            threading.Thread(target=handle_udp, args=[
+                             udp_client_socket, msg_queue]).start()
 
             while not connections_must_be_closed.is_set():
                 pass
-                
+
         close_sockets(client_socket, udp_client_socket)
     except Exception as exc:
         error_message = f'Houve um erro durante a conexão com o servidor | exc: {str(exc)}'
         print(error_message)
         close_sockets(client_socket, udp_client_socket)
-
 
 
 client()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import threading
 import socket
 from collections import namedtuple
-from typing import Dict, List
+from typing import Dict
 
 HOST = '127.0.0.1'
 PORT = 5000
@@ -14,15 +14,19 @@ server.listen()
 Client = namedtuple('Client', ['socket', 'port', 'address', 'nickname'])
 clients: Dict[str, Client] = {}
 
+
 def broadcast(clients: Dict[str, Client], message: str):
     for client_data in clients.values():
         client_data.socket.send(message.encode('ascii'))
 
+
 def broadcast_registry_table(clients: Dict[str, Client]) -> str:
     tabela = 'NICKNAME             ADDRESS             PORT\n'
     for nickname, info in clients.items():
-            tabela = tabela + f'{nickname}             {info.port}             {info.address}\n'
+        tabela = tabela + \
+            f'{nickname}             {info.port}             {info.address}\n'
     broadcast(clients, tabela)
+
 
 def ask_for_client_nickname(client: socket.socket, id: int) -> str:
     msg = 'NICK' if id == 0 else 'NICKNAME_ALREADY_TAKEN'
@@ -31,18 +35,23 @@ def ask_for_client_nickname(client: socket.socket, id: int) -> str:
     print(f'Nickname of the client is {nickname}')
     return nickname
 
+
 def receive_decoded_message(client: socket.socket) -> str:
     message = client.recv(1024)
     return message.decode('ascii')
 
+
 def send_encoded_message(client: socket.socket, message: str):
     client.send(message.encode('ascii'))
+
 
 def nickname_already_taken(nickname: str, clients: Dict[str, Client]) -> bool:
     return nickname in clients.keys()
 
+
 def get_client_by_nickname(nickname: str, clients: Dict[str, Client]) -> Client:
     return clients.get(nickname)
+
 
 def handle(client: Client, clients: Dict[str, Client]):
     while True:
@@ -53,18 +62,19 @@ def handle(client: Client, clients: Dict[str, Client]):
                 nickname_to_remove: str = client.nickname
                 print(f'{nickname_to_remove} disconnected')
                 send_encoded_message(client_socket, 'CONNECTION_CLOSED')
-                
+
                 del clients[nickname_to_remove]
 
                 broadcast(clients, f'{nickname_to_remove} left the chat')
                 broadcast_registry_table(clients)
-                
+
                 break
 
             elif message.split()[0] == '/consulta':
                 nickname = message.split()[1]
                 _client: Client = get_client_by_nickname(nickname, clients)
-                client_msg = (f'QUERY_RESULT|{_client.port}-{_client.address}-{nickname}' if _client else 'NICKNAME_NOT_FOUND')
+                client_msg = (
+                    f'QUERY_RESULT|{_client.port}-{_client.address}-{nickname}' if _client else 'NICKNAME_NOT_FOUND')
                 send_encoded_message(client_socket, client_msg)
 
         except Exception as exc:
@@ -77,29 +87,32 @@ def handle(client: Client, clients: Dict[str, Client]):
             broadcast_registry_table(clients)
             break
 
+
 def receive(clients: Dict[str, Client]):
     while True:
         client, address = server.accept()
         client_port, client_address = address
         print(f'Connected to {str(address)}')
-        
+
         nickname = ask_for_client_nickname(client, 0)
         valid_nickname: bool = not nickname_already_taken(nickname, clients)
-        
+
         while not valid_nickname:
             nickname = ask_for_client_nickname(client, -1)
             valid_nickname = not nickname_already_taken(nickname, clients)
 
-        
-        clients.setdefault(nickname, Client(socket=client, port=client_port, address=client_address, nickname=nickname))
-       
+        clients.setdefault(nickname, Client(
+            socket=client, port=client_port, address=client_address, nickname=nickname))
+
         send_encoded_message(client, 'CLIENT_CONNECTED\n')
 
-        thread = threading.Thread(target=handle, args=(clients[nickname], clients))
+        thread = threading.Thread(
+            target=handle, args=(clients[nickname], clients))
         thread.start()
 
         broadcast_registry_table(clients)
         broadcast(clients, message=f'{nickname} joined the chat.')
+
 
 print('Server listening')
 receive(clients)
